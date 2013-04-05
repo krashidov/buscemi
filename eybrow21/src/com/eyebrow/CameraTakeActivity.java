db4b5ab2697e4a141750d690c339b96a1cedb15e
Koma <Koma@192.168.1.108>
2013-04-05 01:44:04 -0600

    committing all changes since first commit for github

--- eybrow21/src/com/eyebrow/CameraTakeActivity.java
+++ eybrow21/src/com/eyebrow/CameraTakeActivity.java
@@ -20,6 +20,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
@@ -29,84 +30,93 @@ import android.widget.Toast;
 public class CameraTakeActivity extends Activity
 {
 
-	private static final int PICTURE_RESULT = 1;
-	//private FileOutputStream imageFile;
-	private File imageFile;
-	private ImageView pictureHolder;
-	
-	//Paint object to draw stuff
-	private Paint tmpPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-	private Paint Circle = new Paint(Paint.ANTI_ALIAS_FLAG);
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		
+    private static final int PICTURE_RESULT = 1;
+    //private FileOutputStream imageFile;
+    private File imageFile;
+    private ImageView pictureHolder;
+    
+    //Paint object to draw stuff
+    private Paint tmpPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Paint Circle = new Paint(Paint.ANTI_ALIAS_FLAG);
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        
 
-		
-		//The intent here is to capture an image
-		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		
-		//The file will be in the application's directory/savedImages
-		//imageFile = new File(this.getDir("savedImages", MODE_WORLD_WRITEABLE), getFileName());
-		
-		//When we are running the camera app, we want uri to be of the file
-		Uri uri = Uri.fromFile(getTempFile(this));
-		
-		//Pass the intent some parameters...
-		//intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
-		//intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 0);
-		
-		//And we're running the camera app!
-		this.startActivityForResult(intent, PICTURE_RESULT);
-		
-		
-	}
-	
-	//This code runs after the camera app has been completed! Face detection and image manip
-	//Will be done here.
-	public void onActivityResult(int requestCode, int resultCode, Intent data)
-	{
-		if(requestCode == PICTURE_RESULT)
-		{
-			if(resultCode == Activity.RESULT_OK)
-			{
-				Bundle b = data.getExtras();
-				Bitmap pic = (Bitmap) b.get("data");
-				
-				Bitmap nBitmap = pic.copy(Bitmap.Config.RGB_565, true);
-				if(nBitmap != null)
-				{
-			
-					
-					TargetView tView = new TargetView(this);
-					tView.setBitmap(nBitmap);
-					tView.setFaces();
-					setContentView(tView);
-					
-					/*ImageView iv = new ImageView(this);
-					
-					iv.setImageBitmap(nBitmap);
-					setContentView(iv);
-					TextView tv = new TextView(this);
-					
-					String str = Integer.toString(k);
-					/*if(getAllFaces[0] != null)
-						str =Float.toString(myFace.confidence());
-					else
-						str = Integer.toString(nBitmap.getWidth());
-					
-					tv.setText(str);
-					setContentView(tv);*/
-				
-				
-					//Toast.makeText(this, Double.toString(getAllFaces[0].confidence()), Toast.LENGTH_LONG).show();
-				}
-			
-			}
-		}
-	}
+        
+        //The intent here is to capture an image
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        
+        //The file will be in the application's directory/savedImages
+        //imageFile = new File(this.getDir("savedImages", MODE_WORLD_WRITEABLE), getFileName());
+        
+        //When we are running the camera app, we want uri to be of the file
+        Uri uri = Uri.fromFile(getTempFile(this));
+        
+        //Pass the intent some parameters...
+        //intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        //intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 0);
+        
+        //And we're running the camera app!
+        this.startActivityForResult(intent, PICTURE_RESULT);
+        
+        
+    }
+    
+    //This code runs after the camera app has been completed! Face detection and image manip
+    //Will be done here.
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode == PICTURE_RESULT)
+        {
+            if(resultCode == Activity.RESULT_OK)
+            {
+                Bundle b = data.getExtras();
+                Bitmap pic = (Bitmap) b.get("data");
+                
+                Bitmap nBitmap = pic.copy(Bitmap.Config.RGB_565, true);
+                if(nBitmap != null)
+                {
+                    TargetView tView = new TargetView(this);
+                    setContentView(tView);  
+                    LayoutParams l = tView.getLayoutParams();
+                    l.height = 320;
+                   // l.width = 1000;
+                    tView.setLayoutParams(l);
+                    
+                    tView.setBitmap(nBitmap);
+                    if(!tView.setFaces())
+                    {
+                    	Toast t = Toast.makeText(this, "No Faces Found Try again!", Toast.LENGTH_SHORT);
+                    	t.show();
+                    	this.finish();
+                    }
 
-	
+                    
+                    /*ImageView iv = new ImageView(this);
+                    
+                    iv.setImageBitmap(nBitmap);
+                    setContentView(iv);
+                    TextView tv = new TextView(this);
+                    
+                    String str = Integer.toString(k);
+                    /*if(getAllFaces[0] != null)
+                        str =Float.toString(myFace.confidence());
+                    else
+                        str = Integer.toString(nBitmap.getWidth());
+                    
+                    tv.setText(str);
+                    setContentView(tv);*/
+                
+                
+                    //Toast.makeText(this, Double.toString(getAllFaces[0].confidence()), Toast.LENGTH_LONG).show();
+                }
+            
+            }
+        }
+    }
+
+    
     private File getTempFile(Context ctx)
     {
         final File path = new File(Environment.getExternalStorageDirectory(), ctx.getPackageName());
--- eybrow21/src/com/eyebrow/TargetView.java
+++ eybrow21/src/com/eyebrow/TargetView.java
@@ -11,6 +11,7 @@ import android.graphics.Rect;
 import android.media.FaceDetector;
 import android.media.FaceDetector.Face;
 import android.view.View;
+import android.widget.Toast;
 
 public class TargetView extends View {
 	private Bitmap bMap;
@@ -36,7 +37,7 @@ public class TargetView extends View {
 		super(c);
 		
 		tmpPaint.setStyle(Paint.Style.STROKE);
-		tmpPaint.setTextAlign(Paint.Align.CENTER);
+		tmpPaint.setTextAlign(Paint.Align.CENTER); 
 		
 		circlePaint.setStyle(Paint.Style.STROKE);
 		circlePaint.setColor(Color.RED);
@@ -48,7 +49,7 @@ public class TargetView extends View {
 		bMap = b.copy(Bitmap.Config.RGB_565, true);
 	}
 	
-	public void setFaces()
+	public boolean setFaces()
 	{
 		if(bMap != null)
 		{
@@ -60,12 +61,22 @@ public class TargetView extends View {
 			myFace = getAllFaces[0];
 			
 			
+			if(myFace != null)
+			{
+				myFace.getMidPoint(eyesMidPoint);
+				eyesDistance = myFace.eyesDistance();
+			}
+			else
+			{
+				return false;
+			}
 			
-			myFace.getMidPoint(eyesMidPoint);
-			
-			eyesDistance = myFace.eyesDistance();
+			return true;
 		}
+		
+		return false;
 	}
+	
 	
 	public void onDraw(Canvas c)
 	{
@@ -83,56 +94,65 @@ public class TargetView extends View {
 	    float rightEyeX = xStart + ( eyesDistance/2);
 	    
 	    
-	    /*int averageSkinColor = 0;
-	    
-	    for(int j = 0; j < 5; j++)
-	    {
-	    	for(int i = 0; i < 5; i++)
-	    	{
-	    		averageSkinColor += getAverage(bMap.getPixel(xStart-2+j, yStart-10 - i));
-	    	}
-	    }
-	    
-	    averageSkinColor /=25;
-		for(int x = 0; x < transformed.getWidth(); x++){
-			for(int y = 0; y < transformed.getHeight(); y++)
-			{
-				int average = (color.red(transformed.getPixel(x, y)) + color.green(transformed.getPixel(x, y))+ color.blue(transformed.getPixel(x, y)))/3;
-				if(average - averageSkinColor < -65)
-					transformed.setPixel(x,y, Color.BLACK);
-				else
-					transformed.setPixel(x, y, Color.WHITE);
-			}}
-		
-
-
-		transformed.setPixel(leftEyeX, yStart, 255);
-		transformed.setPixel(rightEyeX, yStart, 255);
-		transformed.setPixel(xStart, yStart-10, averageSkinColor);*/
-
-
-		
 		if(myFace!= null)
 		{
 		xRatio = getWidth()*1.0f / bMap.getWidth();
 		yRatio = getHeight()*1.0f / bMap.getHeight();
-		
-		bMap.setPixel((int) leftEyeX, (int) yStart, 255);
-		bMap.setPixel((int) rightEyeX, (int) yStart, 255);
-		
-		c.drawBitmap(bMap, null, new Rect(0,0,getWidth(),getHeight()),tmpPaint);
 		Bitmap lefteye = BitmapFactory.decodeResource(getResources(), R.drawable.left);
 		Bitmap righteye = BitmapFactory.decodeResource(getResources(), R.drawable.right);
-
-		c.drawBitmap(lefteye, (float) (leftEyeX*(c.getHeight()/bMap.getHeight())), (float) (yStart*(c.getHeight()/bMap.getHeight())), tmpPaint);
-		c.drawBitmap(righteye, (float) (c.getWidth()-(rightEyeX *3)), (float) (c.getHeight() - (yStart*6.2)), tmpPaint);
 		
-		c.drawText(Float.toString(leftEyeX), 10, 10, circlePaint);
+		float scale =18;
+		
+		///----------Left Eye-----------------------//
+		float calcStepLeftEyeWidth  =  ((float) lefteye.getWidth())/scale;
+		float calcStepLeftEyeHeight = ((float) lefteye.getHeight())/scale;
+		int leftEyeY = (int) (yStart -  8);
+		
+		for(float j = 0; j < lefteye.getHeight(); j+= calcStepLeftEyeHeight)
+		{
+			int lX = (int) leftEyeX - 8;
+			for(float i = 0 ; i < lefteye.getWidth();  i+= calcStepLeftEyeWidth)
+			{
+				int p = lefteye.getPixel((int)i, (int)j);
+				if (p != Color.TRANSPARENT)
+					bMap.setPixel((int) lX, leftEyeY, p);
+				lX++;
+			}
+			leftEyeY++;
+		}
+		
+		//------------------------------------------//
+		float calcStepRightEyeWidth  =  ((float) righteye.getWidth())/scale;
+		float calcStepRightEyeHeight = ((float) righteye.getHeight())/scale;
+		int rightEyeY = (int) (yStart -  6);
+		
+		for(float j = 0; j < righteye.getHeight(); j+= calcStepRightEyeHeight)
+		{
+			int rX = (int) rightEyeX - 8;
+			for(float i = 0 ; i < righteye.getWidth();  i+= calcStepRightEyeWidth)
+			{
+				int p = righteye.getPixel((int)i, (int)j);
+				if (p != Color.TRANSPARENT)
+					bMap.setPixel((int) rX, rightEyeY, p);
+				rX++;
+			}
+			rightEyeY++;
+		}
+		//bMap.setPixel((int) leftEyeX, (int) yStart, 255);
+		//bMap.setPixel((int) rightEyeX, (int) yStart, 255);
+		
+		c.drawBitmap(bMap, null, new Rect(0,0,getWidth(),getHeight()),tmpPaint);
 
-		c.drawText(Integer.toString(c.getWidth()), 30, 30, circlePaint);
+
+		//c.drawBitmap(lefteye, (float) (leftEyeX*3), (float) (yStart*2.5), tmpPaint);
+		//c.drawBitmap(righteye, (float) (rightEyeX*3), (float) (yStart*2.5), tmpPaint);
+		
+		//c.drawText(Float.toString(leftEyeX), 10, 10, circlePaint);
+
+		/*c.drawText(Integer.toString(c.getWidth()), 30, 30, circlePaint);
 		c.drawText(Integer.toString(bMap.getWidth()), 60, 30, circlePaint);
 		c.drawText(Integer.toString(c.getHeight()), 90, 30, circlePaint);
-		c.drawText(Integer.toString(bMap.getHeight()), 120, 30, circlePaint);
+		c.drawText(Integer.toString(bMap.getHeight()), 120, 30, circlePaint);*/
 
 
 		
